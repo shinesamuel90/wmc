b5dece84d9cd84e59e10b2c78a746f5881df0954
shinesamuel90 <shinesamuel90@gmail.com>
2020-05-06 12:09:37 +0400

    Added android platform

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyApQKCdEWtaLxhAtzeuz9hRwyLgC8ruD9c",
+    authDomain: "insysmemalayalee.firebaseapp.com",
+    databaseURL: "https://insysmemalayalee.firebaseio.com",
+    projectId: "insysmemalayalee",
+    storageBucket: "insysmemalayalee.appspot.com",
+    messagingSenderId: "76443857693",
+    appId: "1:76443857693:web:edd215adc6302d35"
+  }
 };
